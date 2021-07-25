62e3aaf2ba9063944598264d38a1336cd8b57384
Diego Berrios <201503609@ingenieria.usac.edu.gt>
2021-07-25 00:49:35 -0600

    ADD: conexión MongoDb

---
+++ NodeJsServer/mongoConn.js
@@ -0,0 +1,21 @@
+const mongodb = require('mongodb');
+const DB_STR_CON = 'mongodb://rootdev:rootdev@34.134.212.13:27017'
+
+let db = undefined;
+
+function withDB(callback, collection = "users", database = "testdb",) {
+    mongodb.MongoClient.connect(
+        DB_STR_CON,
+        { useUnifiedTopology: true },
+        (err, client) => {
+            if (err) {
+                console.log(err);
+            }
+            db = client.db(database);
+            const coll = db.collection(collection);
+            console.log("working with ", database, ".", collection, "...");
+            callback(coll);
+        });
+}
+
+module.exports = withDB;
